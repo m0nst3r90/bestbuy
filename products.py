fed0cc056dd21dcb999f81d8fdc2c06823a5d5a5
BenjaminHoehns <benjamin.hoehns@yahoo.de>
2026-08-21 19:16:21 +0200

    Storeklasse erstellt Kommentar für Testcase hinzugefügt

--- products.py
+++ products.py
@@ -36,6 +36,8 @@ class Product:
             print(e)
 
 
+
+#Testcase
 bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 mac = Product("MacBook Air M2", price=1450, quantity=100)
 
---
+++ store.py
@@ -0,0 +1,38 @@
+import products
+from products import Product
+
+
+class Store:
+
+    def __init__(self, products:list[Product] = None):
+        self.products: list[Product] = products
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+
+    def remove_product(self, product: Product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        return len(self.products)
+
+    def get_all_products(self) -> list[Product]:
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list:list[tuple[Product, int]]) -> float:
+        order_total: float = 0
+        for product in shopping_list:
+            product2b, quantity2b = product
+            if product2b.is_active():
+                order_total += product2b.buy(quantity2b)
+        return order_total
+
+
+
+#Testcase
+bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = products.Product("MacBook Air M2", price=1450, quantity=100)
+
+best_buy = Store([bose, mac])
+price = best_buy.order([(bose, 5), (mac, 30), (bose, 10)])
+print(f"Order cost: {price} dollars.")
