aeb0fa21d1abc49e53be32ccb5e62e13bec3868f
BenjaminHoehns <benjamin.hoehns@yahoo.de>
2026-08-22 10:41:52 +0200

    Changed order navigation

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ def start(store:store.Store):
                                 cart_product_amount += amount
                         print(f"{i + 1}. {product.name:<26} \033[34m{'$':>2}{product.price:>6}\033[0m\033[36m{product.quantity:>10}\033[0m \033[31m{('- '+str(cart_product_amount)) if cart_product_amount > 0 else ''}\033[0m")
 
-                    print(f"\nEnter \033[31m{'x'}\033[0m to go back to menu")
-                    print(f"When you want to finish order, enter empty text.")
+                    print(f"When you want to finish order or leave, enter empty text.")
                     print(f"Current cart: \033[34m${total_order_cost}\033[0m")
                     userinput = input("\nWich product \033[32m#\033[0m do you want? ")
 
@@ -90,7 +89,7 @@ def start(store:store.Store):
                                     print(f"Product added to list")
                                     time.sleep(1)
                                 else:
-                                    print(f"Amount not available")
+                                    print(f"\033[31mAmount not available\033[0m")
                                     time.sleep(1)
 
 
